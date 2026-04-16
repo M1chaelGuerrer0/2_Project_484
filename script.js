134c61ab01d9638350baaec3dc3d72e5c473f20c
M1chaelGuerrer0 <michael.isaak.guerrero@gmail.com>
2026-04-16 14:08:04 -0700

    added sounds and new button and thirst behavior and rebalancing and refactoring

--- script.js
+++ script.js
@@ -8,22 +8,40 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
     $('.play-button').click(clickedPlayButton);
     $('.exercise-button').click(clickedExerciseButton);
     $('.pet-button').click(clickedPetButton);
-  
-    setInterval(decayHappiness, 5000);  
+    $('.drink-button').click(clickedDrinkButton);
+
+    setInterval(decay, 5000);  
   })
   
     // Add a variable "pet_info" equal to a object with the name (string), weight (number), and happiness (number) of your pet
     var pet_info = {
       name:"Mothra", 
       weight:50, 
-      happiness:50
+      happiness:50,
+      thirst:50
     };
 
+    // used for weight status and effects, feel free to change values and add more stages
+    const weightStages = [
+      { max: 25, status: "Underweight", effect: 0.8, decay: 2 },
+      { max: 45, status: "Fit",         effect: 2, decay: 1 },
+      { max: 65, status: "Normal",      effect: 1.5, decay: 1 },
+      { max: 75, status: "Overweight",  effect: 0.8, decay: 2 },
+      { max: 100, status: "Obese",      effect: 0.5, decay: 3 }
+    ];
+
     var default_pet_image = 'images/mothra_default.png';
+
+    let treatSound = new Audio('sounds/treat_sound.mp3');
+    let playSound = new Audio('sounds/play_sound.mp3');
+    let exerciseSound = new Audio('sounds/exercise_sound.mp3');
+    let petSound = new Audio('sounds/pet_sound.mp3');
+    let drinkSound = new Audio('sounds/drink_sound.mp3');
   
     function clickedTreatButton() {
       pet_info.happiness += Math.round(10 * applyWeightEffects());
       pet_info.weight += 5;
+      treatSound.play();
 
       // .attr() is used to change the image source dynamically 
       $('.pet-image').attr('src', 'images/mothra_treat.png');
@@ -44,6 +62,7 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
     function clickedPlayButton() {
       pet_info.happiness += Math.round(6 * applyWeightEffects());
       pet_info.weight -= 3;
+      playSound.play();
 
       $('.pet-image').attr('src', 'images/mothra_default.png');
       // up and down animation for 5 seconds
@@ -68,7 +87,9 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
     function clickedExerciseButton() {
       pet_info.happiness -= Math.round(5 * applyWeightEffects());
       pet_info.weight -= 6;
-    
+      pet_info.thirst -= 10;
+      exerciseSound.play();
+
       $('.pet-image').attr('src', 'images/mothra_exercise.png');
       
       // left to right animation for 5 seconds
@@ -94,6 +115,7 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
     function clickedPetButton() {
       pet_info.happiness += 1 * applyWeightEffects();
       pet_info.weight += 0.5; // lazy weight added
+      petSound.play();
 
       $('.pet-image').attr('src', 'images/mothra_pet.gif');
       
@@ -108,17 +130,36 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
 
       lockButton();
     }
-  
+
+    function clickedDrinkButton() {
+      pet_info.thirst += 20;
+      pet_info.weight += 1;
+      pet_info.happiness += 2 * applyWeightEffects();
+      drinkSound.play();
+      
+      $('.pet-image').attr('src', 'images/mothra_drink.png');
+      
+      setTimeout(function() {
+        updatePetStatus();
+        $('.pet-image').attr('src', default_pet_image);
+      }, 5000);
+
+      updateMessage("You gave " + pet_info.name + " a drink! Refreshing!");
+      checkAndUpdatePetInfoInHtml();
+      lockButton();
+    }
+
     function checkAndUpdatePetInfoInHtml() {
       checkWeightAndHappinessBeforeUpdating();  
       updatePetInfoInHtml();
     }
     
     function checkWeightAndHappinessBeforeUpdating() {
-      // Add conditional so if weight is lower than zero.
-      if (pet_info.weight < 0) {
-        pet_info.weight = 0;
+      // Add conditional for weight to be between 15 and 100
+      if (pet_info.weight < 15) {
+        pet_info.weight = 15;
       }
+
       if (pet_info.weight > 100) {
         pet_info.weight = 100;
       }
@@ -127,8 +168,13 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
       if (pet_info.happiness < 0) {
         pet_info.happiness = 0;
       }
-      if (pet_info.happiness > 100) {
-        pet_info.happiness = 100;
+
+      // add conditional for thirst to be between 0 and 100
+      if (pet_info.thirst < 0) {
+        pet_info.thirst = 0;
+      }
+      if (pet_info.thirst > 100) {
+        pet_info.thirst = 100;
       }
     }
 
@@ -152,8 +198,10 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
     function updatePetInfoInHtml() {
       $('.name').text(pet_info['name']);
       $('.weight').text(pet_info['weight']);
+      $('.weight-status').text(getWeightStatus());
       $('.happiness').text(pet_info['happiness']);
       $('.effect').text(applyWeightEffects());
+      $('.thirst').text(pet_info.thirst);
     }
     
     function lockButton() {
@@ -166,12 +214,39 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
       }, 5000);
     }
 
+    function getWeightStage() {
+      for (let stage of weightStages) {
+        if (pet_info.weight <= stage.max) {
+          return stage;
+        }
+      }
+    }
+
+    function getWeightStatus() {
+      return getWeightStage().status;
+    }
+
     function applyWeightEffects() {
-      if (pet_info.weight <= 25) return 2.0;
-      if (pet_info.weight < 40) return 1.5;
-      if (pet_info.weight <= 60) return 1.0;
-      if (pet_info.weight <= 75) return 0.5;
-      return 0.25;
+      return getWeightStage().effect;
+    }
+
+    function getThirstPenalty() {
+      if (pet_info.thirst <= 20) return 2;
+      if (pet_info.thirst <= 40) return 1;
+      return 0;
+    }
+
+    function decay() {
+      pet_info.happiness -= getWeightStage().decay + getThirstPenalty();
+      pet_info.thirst -= 3;
+
+      updatePetStatus();
+
+      if (!$('.button-container button').first().prop('disabled')) { // only update if buttons are not locked
+         $('.pet-image').attr('src', default_pet_image); // update image in case pet becomes sad or happy
+      }
+
+      checkAndUpdatePetInfoInHtml();
     }
 
     function updatePetStatus() {
@@ -184,16 +259,4 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
       else {
         default_pet_image = 'images/mothra_default.png';
       }
-    }
-
-    function decayHappiness() {
-      pet_info.happiness -= 1;
-    
-      updatePetStatus();
-
-      if (!$('.button-container button').first().prop('disabled')) { // only update if buttons are not locked
-         $('.pet-image').attr('src', default_pet_image); // update image in case pet becomes sad or happy
-      }
-
-      checkAndUpdatePetInfoInHtml();
     }
